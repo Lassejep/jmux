0130cb8d3939d602d4dcac2c10131816bc9a5544
lassejep <lasse.jepsen@hotmail.com>
2024-09-07 03:59:29 +0200

    added window class

--- jmux.py
+++ jmux.py
@@ -1,4 +1,57 @@
+import pathlib
+import subprocess
+from typing import Optional
+from dataclasses import dataclass
+
+
 class JmuxError(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
         super().__init__(self.message)
+
+
+@dataclass
+class TmuxPane:
+    id: int
+    path: pathlib.Path = pathlib.Path().home()
+    is_active: bool = False
+    processes: Optional[dict] = None
+
+
+class TmuxWindow:
+    def __init__(self, id: int) -> None:
+        self.id: int = id
+        self.name: str = str(id)
+        self.layout: str = None
+        self.is_active: bool = False
+        self.panes: [TmuxPane] = None
+
+    def __get_pane_string_from_tmux(self) -> str:
+        try:
+            tmux_panes = subprocess.check_output(
+                ["tmux", "list-panes", "-t", self.name, "-F",
+                 "#P:#{pane_current_path}:#{pane_active}"],
+            ).decode("utf-8")
+        except subprocess.CalledProcessError as e:
+            raise JmuxError(f"Failed to get panes: {e}")
+        return tmux_panes
+
+    def __create_pane_from_string(self, pane_string: str) -> TmuxPane:
+        pane_id, pane_path, pane_active = pane_string.split(":")
+        pane_id = int(pane_id)
+        pane_path = pathlib.Path(pane_path)
+        pane_active = pane_active == "1"
+        return TmuxPane(pane_id, pane_path, pane_active)
+
+    def __convert_pane_string_to_pane_struct(self, tmux_panes: str) -> [str]:
+        panes = []
+        for line in tmux_panes.split("\n"):
+            if not line:
+                continue
+            pane = self.__create_pane_from_string(line)
+            panes.append(pane)
+        return panes
+
+    def load_panes_from_tmux(self) -> None:
+        tmux_panes = self.__get_pane_string_from_tmux()
+        self.panes = self.__convert_pane_string_to_pane_struct(tmux_panes)
--- test/test_jmux.py
+++ test/test_jmux.py
@@ -1,36 +1,48 @@
 import unittest
+import pathlib
+import subprocess
+
 import jmux
 
 
 class TestTmuxPane(unittest.TestCase):
-    def setUp(self):
-        self.pane_data = {
-            "1": {
-                "is_active": True,
-                "current_path": "~/",
-                "processes": [
-                    {
-                        "pid": 1234,
-                        "name": "bash",
-                        "command": "bash",
-                        "args": []
-                    }
-                ]
-            }
-        }
-
-    def tearDown(self):
-        pass
-
-    def test_pane_creation(self):
+    def test_pane_object_creation(self):
         try:
-            jmux.TmuxPane()
+            pane = jmux.TmuxPane(1)
         except Exception as e:
             self.fail("TmuxPane creation failed: " + str(e))
+        self.assertEqual(pane.id, 1)
+        self.assertEqual(pane.path, pathlib.Path().home())
+        self.assertFalse(pane.is_active)
+        self.assertFalse(pane.processes)
 
-    def test_pane_load_from_tmux(self):
+
+class TestTmuxWindow(unittest.TestCase):
+    def setUp(self):
+        subprocess.run(["tmux", "new-session", "-d",
+                       "-s", "jmux_test"], check=True)
+        subprocess.run(["tmux", "rename-window", "-t",
+                       "jmux_test:0", "1"], check=True)
+        subprocess.run(["tmux", "split-window", "-t",
+                       "jmux_test:0", "-h"], check=True)
+        subprocess.run(["tmux", "split-window", "-t",
+                       "jmux_test:0", "-v"], check=True)
+
+    def tearDown(self):
+        subprocess.run(["tmux", "kill-session", "-t", "jmux_test"], check=True)
+
+    def test_window_object_creation(self):
         try:
-            pane = jmux.TmuxPane()
-            pane.load_from_tmux()
+            window = jmux.TmuxWindow(1)
         except Exception as e:
-            self.fail("TmuxPane load_from_tmux failed: " + str(e))
+            self.fail("TmuxWindow creation failed: " + str(e))
+        self.assertEqual(window.id, 1)
+        self.assertEqual(window.name, "1")
+        self.assertFalse(window.layout)
+        self.assertFalse(window.is_active)
+        self.assertFalse(window.panes)
+
+    def test_window_load_panes_from_tmux(self):
+        window = jmux.TmuxWindow(1)
+        window.load_panes_from_tmux()
+        self.assertNotEqual(len(window.panes), 3)
