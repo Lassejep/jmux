191e74a0af2ac1f47a07ade4509aeb3d71392718
lassejep <lasse.jepsen@hotmail.com>
2024-09-13 04:50:53 +0200

    refactor

--- src/session_manager.py
+++ src/session_manager.py
@@ -1,7 +1,8 @@
 import pathlib
 import subprocess
 import json
-from typing import List
+import abc
+from typing import List, Self
 from dataclasses import dataclass, asdict
 
 
@@ -11,28 +12,120 @@ class JmuxError(Exception):
         super().__init__(self.message)
 
 
+class TmuxBin():
+    def __init__(self) -> None:
+        self.tmux_bin = self._find_tmux_bin()
+
+    def run(self, cmd: List[str]) -> str:
+        try:
+            cmd.insert(0, self.tmux_bin)
+            return subprocess.check_output(cmd, text=True).strip()
+        except subprocess.CalledProcessError as e:
+            raise JmuxError(f"Failed to run command: {e}")
+
+    def _find_tmux_bin(self) -> str:
+        try:
+            return subprocess.check_output(["which", "tmux"],
+                                           text=True).strip()
+        except subprocess.CalledProcessError as e:
+            raise JmuxError(f"Failed to find tmux binary: {e}")
+
+    def get(self, keys: str | List[str], target: str = "") -> str:
+        cmd = ["display-message"]
+        if target:
+            cmd.extend(["-t", target])
+        if type(keys) is not list:
+            keys = [keys]
+        keystr = ""
+        for key in keys:
+            keystr += f"#{{{key}}}:"
+        cmd.extend(["-p", f"{keystr[:-1]}"])
+        output = self.run(cmd).split(":")
+        if len(output) == 1:
+            return output[0]
+        return output
+
+
+class ITmuxElement(abc.ABC):
+    def __init__(self) -> None:
+        self.tmux = TmuxBin()
+
+    @abc.abstractmethod
+    def build_from_tmux(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def create_in_tmux(self) -> None:
+        pass
+
+
 @dataclass
-class TmuxPane:
+class TmuxPane(ITmuxElement):
     id: int
     path: str
     is_active: bool
+    tmux: TmuxBin = TmuxBin()
+
+    def build_from_tmux(self, session_name: str, window_index: int,
+                        pane_index: int) -> Self:
+        keys = ["pane_index", "pane_current_path", "pane_active"]
+        target = f"{session_name}:{window_index}.{pane_index}"
+        pane_id, path, is_active = self.tmux.get(keys, target)
+        return TmuxPane(int(pane_id), path, is_active == "1")
+
+    def create_in_tmux(self, session_name: str, window_index: int) -> None:
+        if self.id == 1:
+            cmd = ["send-keys", "-t",
+                   f"{session_name}:{window_index}.{self.id}",
+                   f"cd {self.path}", "Enter", "clear", "Enter"]
+        else:
+            cmd = ["splitw", "-t",
+                   f"{session_name}:{window_index}", "-c", self.path]
+            if not self.is_active:
+                cmd.append("-d")
+        self.tmux.run(cmd)
 
 
 @dataclass
-class TmuxWindow:
+class TmuxWindow(ITmuxElement):
     id: int
     name: str
     is_active: bool
     layout: str
     panes: List[TmuxPane]
+    tmux: TmuxBin = TmuxBin()
 
     def __post_init__(self) -> None:
         if type(self.panes[0]) is dict:
             self.panes = [TmuxPane(**pane) for pane in self.panes]
 
+    def build_from_tmux(self, session_name: str, window_index: int) -> Self:
+        keys = ["window_name", "window_active",
+                "window_layout", "window_panes"]
+        target = f"{session_name}:{window_index}"
+        window_name, is_active, layout, window_panes = self.tmux.get(
+            keys, target)
+        panes = []
+        for pane_index in range(1, int(window_panes) + 1):
+            panes.append(TmuxPane.build_from_tmux(
+                session_name, window_index, pane_index))
+        return TmuxWindow(window_index, window_name, is_active == "1", layout, panes)
+
+    def create_in_tmux(self, session_name: str) -> None:
+        cmd = ["neww", "-t", f"{session_name}:{self.id}",
+               "-k", "-n", self.name]
+        if not self.is_active:
+            cmd.append("-d")
+        self.tmux.run(cmd)
+        for pane in self.panes:
+            pane.create_in_tmux(session_name, self.id, pane)
+        cmd = ["select-layout", "-t",
+               f"{session_name}:{self.id}", self.layout]
+        self.tmux.run(cmd)
+
 
 @dataclass
-class TmuxSession:
+class TmuxSession(ITmuxElement):
     name: str
     windows: List[TmuxWindow]
 
@@ -40,88 +133,29 @@ class TmuxSession:
         if type(self.windows[0]) is dict:
             self.windows = [TmuxWindow(**window) for window in self.windows]
 
+    def build_from_tmux(self, session_name: str) -> Self:
+        keys = ["session_windows"]
+        target = f"{session_name}"
+        session_windows = self.tmux.get(keys, target)
+        windows = []
+        for window_index in range(1, int(session_windows) + 1):
+            windows.append(TmuxWindow.build_from_tmux(
+                session_name, window_index))
+        return TmuxSession(session_name, windows)
+
+    def create_in_tmux(self) -> None:
+        cmd = ["new-session", "-d", "-s", self.name]
+        self.tmux.run(cmd)
+        for window in self.windows:
+            window.create_in_tmux(self.name, window)
+
 
 class TmuxManager:
     def __init__(self) -> None:
-        self._tmux_bin = self._find_tmux_bin()
+        self.tmux = TmuxBin()
         self._sessions_dir = pathlib.Path().home() / ".config" / "jmux"
         if not self._sessions_dir.exists():
             self._sessions_dir.mkdir()
-
-    def _run_cmd(self, cmd: List[str]) -> str:
-        try:
-            return subprocess.check_output(cmd, text=True).strip()
-        except subprocess.CalledProcessError as e:
-            raise JmuxError(f"Failed to run command: {e}")
-
-    def _find_tmux_bin(self) -> str:
-        return self._run_cmd(["which", "tmux"])
-
-    def _get_pane(self, session_name: str, window_index: int,
-                  pane_index: int) -> TmuxPane:
-        cmd = [self._tmux_bin, "display-message", "-t",
-               f"{session_name}:{window_index}.{pane_index}", "-p",
-               "#P:#{pane_current_path}:#{pane_active}"]
-        pane_id, path, is_active = self._run_cmd(cmd).split(":")
-        return TmuxPane(int(pane_id), path, is_active == "1")
-
-    def _get_window(self, session_name: str, window_index: int) -> TmuxWindow:
-        cmd = [self._tmux_bin, "display-message", "-t",
-               f"{session_name}:{window_index}", "-p",
-               "#W:#{window_active}:#{window_layout}:#{window_panes}"]
-        window_name, is_active, layout, window_panes = self._run_cmd(
-            cmd).split(":")
-        panes = []
-        for pane_index in range(1, int(window_panes) + 1):
-            panes.append(self._get_pane(
-                session_name, window_index, pane_index))
-        return TmuxWindow(window_index, window_name, is_active == "1", layout,
-                          panes)
-
-    def _get_session(self, session_name: str) -> TmuxSession:
-        cmd = [self._tmux_bin, "display-message",
-               "-t", f"{session_name}", "-p", "#{session_windows}"]
-        session_windows = self._run_cmd(cmd)
-        windows = []
-        for window_index in range(1, int(session_windows) + 1):
-            windows.append(self._get_window(session_name, window_index))
-        return TmuxSession(session_name, windows)
-
-    def get_current_session(self) -> TmuxSession:
-        cmd = [self._tmux_bin, "display-message", "-p", "#{session_name}"]
-        session_name = self._run_cmd(cmd)
-        return self._get_session(session_name)
-
-    def _create_pane(self, session_name: str, window_index: int,
-                     pane: TmuxPane) -> None:
-        if pane.id == 1:
-            cmd = [self._tmux_bin, "send-keys", "-t",
-                   f"{session_name}:{window_index}.{pane.id}",
-                   f"cd {pane.path}", "Enter", "clear", "Enter"]
-        else:
-            cmd = [self._tmux_bin, "splitw", "-t",
-                   f"{session_name}:{window_index}", "-c", pane.path]
-            if not pane.is_active:
-                cmd.append("-d")
-        self._run_cmd(cmd)
-
-    def _create_window(self, session_name: str, window: TmuxWindow) -> None:
-        cmd = [self._tmux_bin, "neww", "-t", f"{session_name}:{window.id}",
-               "-k", "-n", window.name]
-        if not window.is_active:
-            cmd.append("-d")
-        self._run_cmd(cmd)
-        for pane in window.panes:
-            self._create_pane(session_name, window.id, pane)
-        cmd = [self._tmux_bin, "select-layout", "-t",
-               f"{session_name}:{window.id}", window.layout]
-        self._run_cmd(cmd)
-
-    def create_session(self, session: TmuxSession) -> None:
-        cmd = [self._tmux_bin, "new-session", "-d", "-s", session.name]
-        self._run_cmd(cmd)
-        for window in session.windows:
-            self._create_window(session.name, window)
 
     def save_session(self, session: TmuxSession) -> None:
         with open(self._sessions_dir / f"{session.name}.json", "w") as f:
@@ -130,5 +164,13 @@ class TmuxManager:
     def load_session(self, session_name: str) -> TmuxSession:
         with open(self._sessions_dir / f"{session_name}.json") as f:
             session = TmuxSession(**json.load(f))
-        self.create_session(session)
+        session.create_in_tmux()
         return session
+
+    def get_current_session(self) -> TmuxSession:
+        session_name = self.tmux.get("session_name")
+        return TmuxSession.build_from_tmux(session_name)
+
+    def save_current_session(self) -> None:
+        session = self.get_current_session()
+        self.save_session(session)
--- test/test_jmux.py
+++ test/test_jmux.py
@@ -2,89 +2,93 @@ import unittest
 import subprocess
 import json
 
-from src.session_manager import TmuxManager, TmuxSession, TmuxWindow, TmuxPane
+from src.session_manager import TmuxManager
+from src.session_manager import TmuxPane, TmuxWindow, TmuxSession, TmuxBin
 
 
-class TestTmuxManager(unittest.TestCase):
-    def setUp(self):
+def create_test_session():
+    subprocess.run(["tmux", "new-session", "-d",
+                   "-s", "test_session"], check=True)
+    subprocess.run(["tmux", "rename-window", "-t",
+                   "test_session:1", "test_window1"], check=True)
+    subprocess.run(["tmux", "new-window", "-t",
+                   "test_session", "-n", "test_window2"], check=True)
+    subprocess.run(["tmux", "new-window", "-t",
+                   "test_session", "-n", "test_window2"], check=True)
+    subprocess.run(["tmux", "split-window", "-h",
+                   "-t", "test_session:1", "-c", "/tmp"], check=True)
+    subprocess.run(["tmux", "split-window", "-v",
+                   "-t", "test_session:1.1"], check=True)
+    subprocess.run(["tmux", "split-window", "-v",
+                   "-t", "test_session:2.1"], check=True)
+
+
+def kill_test_session():
+    try:
+        subprocess.run(["tmux", "kill-session", "-t", "test_session"],
+                       check=True, stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
         pass
 
+
+class TestTmuxBin(unittest.TestCase):
     def tearDown(self):
-        try:
-            subprocess.run(["tmux", "kill-session", "-t", "test_session"],
-                           check=True, stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
-        except subprocess.CalledProcessError:
-            pass
+        kill_test_session()
 
-    def create_test_session(self):
-        subprocess.run(["tmux", "new-session", "-d",
-                       "-s", "test_session"], check=True)
-        subprocess.run(["tmux", "rename-window", "-t",
-                       "test_session:1", "test_window1"], check=True)
-        subprocess.run(["tmux", "new-window", "-t",
-                       "test_session", "-n", "test_window2"], check=True)
-        subprocess.run(["tmux", "new-window", "-t",
-                       "test_session", "-n", "test_window2"], check=True)
-        subprocess.run(["tmux", "split-window", "-h",
-                       "-t", "test_session:1", "-c", "/tmp"], check=True)
-        subprocess.run(["tmux", "split-window", "-v",
-                       "-t", "test_session:1.1"], check=True)
-        subprocess.run(["tmux", "split-window", "-v",
-                       "-t", "test_session:2.1"], check=True)
+    def test_get(self):
+        create_test_session()
+        tmux = TmuxBin()
+        keys = ["session_windows"]
+        target = "test_session"
+        result = tmux.get(keys, target)
+        self.assertEqual(result, "3")
 
-    def test_get_current_session(self):
-        self.create_test_session()
-        tm = TmuxManager()
-        try:
-            session = tm._get_session("test_session")
-        except Exception:
-            self.fail("Failed to get current session")
-        self.assertEqual(session.name, "test_session")
-        self.assertEqual(len(session.windows), 3)
-        self.assertEqual(session.windows[0].name, "test_window1")
-        self.assertEqual(len(session.windows[0].panes), 3)
-        self.assertEqual(session.windows[1].name, "test_window2")
-        self.assertEqual(len(session.windows[1].panes), 2)
-        self.assertEqual(session.windows[2].name, "test_window2")
-        self.assertEqual(len(session.windows[2].panes), 1)
+    def test_get_multiple_keys(self):
+        create_test_session()
+        tmux = TmuxBin()
+        keys = ["session_windows", "session_name"]
+        target = "test_session"
+        session_windows, session_name = tmux.get(keys, target)
+        self.assertEqual(session_name, "test_session")
+        self.assertEqual(session_windows, "3")
 
-    def test_create_session(self):
-        panes = [
-            TmuxPane(1, "/tmp", True),
-            TmuxPane(2, "/tmp", False),
-            TmuxPane(3, "/tmp", False),
-        ]
-        window1 = TmuxWindow(1, "test_window1", True, "tiled", panes)
-        panes = [
-            TmuxPane(1, "/tmp", True),
-            TmuxPane(2, "/tmp", False),
-        ]
-        window2 = TmuxWindow(2, "test_window2", False, "tiled", panes)
-        panes = [
-            TmuxPane(1, "/tmp", True),
-        ]
-        window3 = TmuxWindow(3, "test_window2", False, "tiled", panes)
-        session = TmuxSession("test_session", [window1, window2, window3])
-        tm = TmuxManager()
-        tm.create_session(session)
+    def test_run(self):
+        tmux = TmuxBin()
+        cmd = ["new-session", "-d", "-s", "test_session"]
+        tmux.run(cmd)
         sessions = subprocess.check_output(["tmux", "list-sessions"],
                                            text=True).strip()
         self.assertIn("test_session", sessions)
-        windows = subprocess.check_output(["tmux", "list-windows", "-t",
-                                          "test_session"], text=True).strip()
-        self.assertIn("test_window1", windows)
-        self.assertIn("test_window2", windows)
-        self.assertEqual(windows.count("\n") + 1, 3)
-        panes = subprocess.check_output(["tmux", "list-panes", "-t",
-                                        "test_session:1"], text=True).strip()
-        self.assertEqual(panes.count("\n") + 1, 3)
-        panes = subprocess.check_output(["tmux", "list-panes", "-t",
-                                        "test_session:2"], text=True).strip()
-        self.assertEqual(panes.count("\n") + 1, 2)
-        panes = subprocess.check_output(["tmux", "list-panes", "-t",
-                                        "test_session:3"], text=True).strip()
-        self.assertEqual(panes.count("\n") + 1, 1)
+
+
+class TestTmuxPane(unittest.TestCase):
+    def tearDown(self):
+        kill_test_session()
+
+    def test_build_from_tmux(self):
+        create_test_session()
+        pane = TmuxPane(1, "/tmp", True)
+        pane.build_from_tmux("test_session", 1, 1)
+        self.assertEqual(pane.id, 1)
+        self.assertEqual(pane.path, "/tmp")
+        self.assertEqual(pane.is_active, True)
+
+    def test_create_in_tmux(self):
+        create_test_session()
+        pane = TmuxPane(1, "/tmp", True)
+        pane.create_in_tmux("test_session", 2)
+        result = subprocess.check_output(["tmux", "display-message", "-t",
+                                          "test_session:2.1", "-p",
+                                          "#{pane_index}:#{pane_current_path}:#{pane_active}:#{session_name}:#{window_index}"
+                                          ], text=True).strip()
+        print(result)
+        self.assertIn("/tmp", result)
+
+
+class TestTmuxManager(unittest.TestCase):
+    def tearDown(self):
+        kill_test_session()
 
     def test_save_session(self):
         panes = [
