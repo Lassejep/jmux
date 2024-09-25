6e57d81f4b423cf34401dd8cc66aecf359706ae5
lassejep <lasse.jepsen@hotmail.com>
2024-09-25 13:34:36 +0200

    jmux elements rewrite to fit the new MultiplexerAPI interface

--- src/elements.py
+++ src/elements.py
@@ -28,8 +28,7 @@ class JmuxSession:
 
 class JmuxLoader:
     """
-    Load terminal multiplexer sessions, windows, and panes
-    into Jmux objects.
+    Load terminal multiplexer sessions, windows, and panes into Jmux objects.
     """
 
     def __init__(self, multiplexer: TerminalMultiplexerAPI):
@@ -37,12 +36,48 @@ class JmuxLoader:
         if multiplexer is None:
             raise ValueError("Invalid multiplexer")
 
-    def load_panes(self, target: str) -> list[JmuxPane]:
-        if not target:
-            raise ValueError("Invalid target")
+    def load(self) -> JmuxSession:
+        """
+        Load the current session into a JmuxSession object.
+        """
+        if not self.multiplexer.is_running():
+            raise EnvironmentError("Not in a session")
+        session = self._load_session()
+        return session
+
+    def _load_session(self) -> JmuxSession:
+        session_data = self.multiplexer.get(
+            ["session_name", "session_id", "windows"])
+        windows = self._load_windows(session_data["session_id"],
+                                     session_data["windows"])
+        return JmuxSession(name=session_data["session_name"],
+                           id=session_data["session_id"], windows=windows)
+
+    def _load_windows(self, session_id: str,
+                      num_windows: int) -> list[JmuxWindow]:
+        windows = []
+        for i in range(int(num_windows)):
+            window_data = self.multiplexer.get(["window_name", "window_id",
+                                                "layout", "window_focus",
+                                                "panes"], f"{session_id}:{i}")
+            panes = self._load_panes(window_data["window_id"],
+                                     window_data["panes"])
+            windows.append(JmuxWindow(name=window_data["window_name"],
+                                      id=window_data["window_id"],
+                                      layout=window_data["layout"],
+                                      focus=window_data["window_focus"],
+                                      panes=panes))
+        return windows
+
+    def _load_panes(self, window_id: str, num_panes: int) -> list[JmuxPane]:
         panes = []
-        pane_ids = self.multiplexer.get(["pane_id"], target)
-        print(pane_ids)
+        for i in range(int(num_panes)):
+            keys = ["pane_id", "pane_focus", "pane_current_dir"]
+            pane_data = self.multiplexer.get(keys, f"{window_id}.{i}")
+            pane = JmuxPane(id=pane_data["pane_id"],
+                            focus=int(pane_data["pane_focus"]) == 1,
+                            current_dir=pane_data["pane_current_dir"])
+            panes.append(pane)
         return panes
 
 
--- test/test_jmux_elements.py
+++ test/test_jmux_elements.py
@@ -1,46 +1,96 @@
 import pytest
-import subprocess
 
-from src.multiplexer import TmuxAPI
-from src.elements import JmuxPane
+from src.elements import JmuxSession, JmuxWindow, JmuxPane
 from src.elements import JmuxLoader
+
+
+@pytest.fixture()
+def mock_tmux_api(mocker):
+    pane = JmuxPane("pane_id", True, "/home/user")
+    window = JmuxWindow("test_window", "window_id", "tiled", True, [pane])
+    session = JmuxSession("test_session", "session_id", [window])
+    mock_tmux = mocker.patch('src.multiplexer.TmuxAPI')
+    mock_tmux.is_running.return_value = True
+    tmux_data = {"session_name": session.name, "session_id": session.id,
+                 "windows": len(session.windows), "window_name": window.name,
+                 "window_id": window.id, "layout": window.layout,
+                 "window_focus": window.focus, "panes": len(window.panes),
+                 "pane_id": pane.id, "pane_focus": pane.focus,
+                 "pane_current_dir": pane.current_dir}
+    mock_tmux.get.return_value = tmux_data
+    mock_tmux.create_session.return_value = session.id
+    mock_tmux.create_window.return_value = window.id
+    mock_tmux.create_pane.return_value = pane.id
+    return mock_tmux
+
+
+@pytest.fixture()
+def tmux_api(mock_tmux_api):
+    yield mock_tmux_api
+
+
+@pytest.fixture()
+def jmux_loader(tmux_api):
+    yield JmuxLoader(tmux_api)
+
+
+@pytest.fixture()
+def session(tmux_api):
+    session_id = tmux_api.create_session("test_session")
+    yield session_id
+
+
+@pytest.fixture()
+def window(tmux_api, session):
+    window_id = tmux_api.create_window("test_window", session)
+    yield window_id
+
+
+@pytest.fixture()
+def pane(tmux_api, window):
+    pane_id = tmux_api.create_pane(window)
+    yield pane_id
 
 
 class TestJmuxLoader:
     """Test the JmuxLoader class."""
 
     def test_throws_exception_given_invalid_multiplexer(self):
-        """Test that an exception is thrown if the multiplexer is invalid."""
         with pytest.raises(ValueError):
             JmuxLoader(None)
 
+    def test_load_returns_jmux_session(self, jmux_loader, session):
+        jmux_session = jmux_loader.load()
+        assert isinstance(jmux_session, JmuxSession)
 
-class TestJmuxLoaderLoadPanesMethod:
-    """Test the JmuxLoader.load_panes method."""
+    def test_load_throws_exception_if_called_outside_of_a_session(
+            self, jmux_loader, mocker):
+        mocker.patch.object(jmux_loader.multiplexer, 'is_running',
+                            return_value=False)
+        with pytest.raises(EnvironmentError):
+            jmux_loader.load()
 
-    def test_load_from_no_target_throws_exception(self):
-        """Test that an exception is thrown if the target is invalid."""
-        loader = JmuxLoader(TmuxAPI(subprocess))
-        with pytest.raises(ValueError):
-            loader.load_panes("")
+    def test_load_returns_jmux_session_with_current_session_id(
+            self, jmux_loader, session):
+        jmux_session = jmux_loader.load()
+        assert jmux_session.id == session
 
-    def test_load_from_valid_target_returns_list_of_panes(self):
-        """Test that the method returns a list of JmuxPane objects."""
-        loader = JmuxLoader(TmuxAPI(subprocess))
-        panes = loader.load_panes("@1")
-        assert all(isinstance(pane, JmuxPane) for pane in panes)
+    def test_load_returns_jmux_session_with_current_session_name(
+            self, jmux_loader, session):
+        jmux_session = jmux_loader.load()
+        assert jmux_session.name == "test_session"
 
-    def test_load_from_target_with_one_pane_returns_list_with_one_pane(self):
-        """Test that the method returns a list with one JmuxPane object."""
-        loader = JmuxLoader(TmuxAPI(subprocess))
-        panes = loader.load_panes("@1")
-        assert len(panes) == 1
-        assert isinstance(panes[0], JmuxPane)
+    def test_load_returns_jmux_session_with_current_windows(
+            self, jmux_loader, window):
+        jmux_session = jmux_loader.load()
+        assert jmux_session.windows[0].id == window
 
-    def test_load_from_target_with_multiple_panes_returns_list_with_all_panes(
-            self):
-        """Test that the method returns a list with all JmuxPane objects."""
-        loader = JmuxLoader(TmuxAPI(subprocess))
-        panes = loader.load_panes("@1")
-        assert len(panes) == 2
-        assert all(isinstance(pane, JmuxPane) for pane in panes)
+    def test_load_returns_jmux_session_with_current_window_name(
+            self, jmux_loader, window):
+        jmux_session = jmux_loader.load()
+        assert jmux_session.windows[0].name == "test_window"
+
+    def test_load_returns_jmux_session_with_current_panes(
+            self, jmux_loader, pane):
+        jmux_session = jmux_loader.load()
+        assert jmux_session.windows[0].panes[0].id == pane
