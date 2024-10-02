657f4a5a2fc43a532dedc4ce3db60360cc5a1633
lassejep <lasse.jepsen@hotmail.com>
2024-10-02 15:19:55 +0200

    updated main script to work with the new SessionManager

--- jmux.py
+++ jmux.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
+import subprocess
+import pathlib
+
+from src import tmux_client
+from src import elements
 from src import session_manager
 
 
-def main(args):
-    tm = session_manager.TmuxManager()
-    if args.run == "save":
-        tm.save_current_session()
-    elif args.run == "load":
-        session_name = input("Enter session name: ")
-        tm.load_session(session_name)
-    elif args.run == "list":
-        for session in tm.list_sessions():
-            print(session)
-    else:
-        raise ValueError(f"Invalid action: {args.run}")
+def getfilepath(name):
+    jmux_dir = pathlib.Path.home() / ".jmux"
+    if not jmux_dir.exists():
+        jmux_dir.mkdir()
+    path = jmux_dir / f"{name}.json"
+    return path
+
+
+def create_manager():
+    tmux = tmux_client.TmuxClient(subprocess)
+    loader = elements.JmuxLoader(tmux)
+    builder = elements.JmuxBuilder(tmux)
+    return session_manager.SessionManager(loader, builder)
+
+
+def main(action):
+    manager = create_manager()
+    match action:
+        case "save":
+            file_path = getfilepath("current")
+            manager.save_session(file_path)
+        case "load":
+            session_name = input("Enter session name: ")
+            file_path = getfilepath(session_name)
+            manager.load_session(file_path)
+        case _:
+            raise ValueError(f"Invalid action: {action}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("run", choices=("save", "load", "list"),
+    parser.add_argument("run", choices=("save", "load"),
                         help="Action to perform")
     args = parser.parse_args()
-    main(args)
+    main(args.run)
