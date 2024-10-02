42fcb7e647724d3b2742ac0890f06e2a8eef09ce
lassejep <lasse.jepsen@hotmail.com>
2024-10-02 14:14:22 +0200

    added load_session method to SessionManager class

--- src/session_manager.py
+++ src/session_manager.py
@@ -2,7 +2,8 @@ import pathlib
 import json
 from dataclasses import asdict
 
-from src.elements import JmuxLoader
+from src.elements import JmuxLoader, JmuxBuilder
+from src.models import JmuxSession, JmuxWindow, JmuxPane
 
 
 class SessionManager:
@@ -10,14 +11,17 @@ class SessionManager:
     Manage terminal multiplexer sessions.
     """
 
-    def __init__(self, jmux_loader: JmuxLoader) -> None:
+    def __init__(self, jmux_loader: JmuxLoader,
+                 jmux_builder: JmuxBuilder) -> None:
         self.jmux_loader = jmux_loader
-        self._check_jmux_loader()
-
-    def _check_jmux_loader(self) -> None:
         if self.jmux_loader is None or not isinstance(
                 self.jmux_loader, JmuxLoader):
             raise ValueError("Invalid JmuxLoader")
+
+        self.jmux_builder = jmux_builder
+        if self.jmux_builder is None or not isinstance(
+                self.jmux_builder, JmuxBuilder):
+            raise ValueError("Invalid JmuxBuilder")
 
     def save_session(self, save_file: pathlib.Path) -> None:
         """
@@ -33,3 +37,30 @@ class SessionManager:
         session_dict = asdict(current_session)
         with save_file.open("w") as file:
             json.dump(session_dict, file, indent=4)
+
+    def load_session(self, load_file: pathlib.Path) -> None:
+        """
+        Load the session from `file_path`.
+        """
+        if not isinstance(load_file, pathlib.Path):
+            raise TypeError("Invalid file path")
+
+        if not load_file.exists():
+            raise FileNotFoundError("File does not exist")
+
+        with load_file.open("r") as file:
+            session_data = json.load(file)
+        session = self._deserialize_session(session_data)
+        self.jmux_builder.build(session)
+
+    def _deserialize_session(self, session_data: dict) -> JmuxSession:
+        session_data["windows"] = [self._deserialize_window(
+            window) for window in session_data["windows"]]
+        session = JmuxSession(**session_data)
+        return session
+
+    def _deserialize_window(self, window_data: dict) -> JmuxWindow:
+        window_data["panes"] = [JmuxPane(**pane)
+                                for pane in window_data["panes"]]
+        window = JmuxWindow(**window_data)
+        return window
--- test/test_session_manager.py
+++ test/test_session_manager.py
@@ -8,8 +8,8 @@ from src.session_manager import SessionManager
 
 
 @pytest.fixture
-def session_manager(mock_loader):
-    yield SessionManager(mock_loader)
+def session_manager(mock_loader, mock_builder):
+    yield SessionManager(mock_loader, mock_builder)
 
 
 @pytest.fixture
@@ -22,9 +22,23 @@ def mock_file(mocker):
 
 
 class TestSessionManagerConstructor:
-    def test_throws_exception_if_jmux_loader_is_none(self):
+    def test_throws_exception_if_jmux_loader_is_none(self, mock_builder):
         with pytest.raises(ValueError):
-            SessionManager(None)
+            SessionManager(None, mock_builder)
+
+    def test_throws_exception_if_jmux_loader_is_not_jmux_loader(
+            self, mock_builder):
+        with pytest.raises(ValueError):
+            SessionManager(mock_builder, mock_builder)
+
+    def test_throws_exception_if_jmux_builder_is_none(self, mock_loader):
+        with pytest.raises(ValueError):
+            SessionManager(mock_loader, None)
+
+    def test_throws_exception_if_jmux_builder_is_not_jmux_builder(
+            self, mock_loader):
+        with pytest.raises(ValueError):
+            SessionManager(mock_loader, mock_loader)
 
 
 class TestSaveSessionMethod:
@@ -60,3 +74,38 @@ class TestSaveSessionMethod:
         written_data = mock_file.open().write.call_args_list
         written_data = ''.join([arg[0][0] for arg in written_data])
         assert written_data == json.dumps(asdict(test_jmux_session), indent=4)
+
+
+class TestLoadSessionMethod:
+    def test_throws_exception_if_file_path_is_not_pathlib_path(
+            self, session_manager):
+        with pytest.raises(TypeError):
+            session_manager.load_session("test_path")
+
+    def test_throws_exception_if_file_does_not_exist(
+            self, session_manager, mock_file):
+        mock_file.exists.return_value = False
+        with pytest.raises(FileNotFoundError):
+            session_manager.load_session(mock_file)
+
+    def test_opens_correct_file(
+            self, session_manager, mock_file, test_jmux_session):
+        mock_file.open().read.return_value = json.dumps(
+            asdict(test_jmux_session), indent=4)
+        session_manager.load_session(mock_file)
+        mock_file.open.assert_called_with("r")
+
+    def test_reads_file_data(
+            self, session_manager, mock_file, test_jmux_session):
+        mock_file.open().read.return_value = json.dumps(
+            asdict(test_jmux_session), indent=4)
+        session_manager.load_session(mock_file)
+        mock_file.open().read.assert_called_once()
+
+    def test_builds_jmux_session_from_file_data(
+            self, session_manager, mock_file, test_jmux_session):
+        mock_file.open().read.return_value = json.dumps(
+            asdict(test_jmux_session), indent=4)
+        session_manager.load_session(mock_file)
+        session_manager.jmux_builder.build.assert_called_once_with(
+            test_jmux_session)
