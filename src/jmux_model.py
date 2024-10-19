15e8e222980ecf95a0f7c89eab82fba346a472fb
lassejep <lasse.jepsen@hotmail.com>
2024-10-19 03:47:27 +0200

    changes to input types in model

--- src/jmux_model.py
+++ src/jmux_model.py
@@ -1,29 +1,32 @@
 from typing import List
 
+from src.file_handler import FileHandler
 from src.jmux_session import SessionLabel
 from src.model import Model
+from src.multiplexer import Multiplexer
 
 
 class JmuxModel(Model):
-    def __init__(self):
-        pass
+    def __init__(self, multiplexer: Multiplexer, file_handler: FileHandler) -> None:
+        self.multiplexer = multiplexer
+        self.file_handler = file_handler
 
     def create_session(self, session_name: str) -> None:
         pass
 
-    def save_session(self, session_name: str) -> None:
+    def save_session(self, label: SessionLabel) -> None:
         pass
 
-    def load_session(self, session_name: str) -> None:
+    def load_session(self, label: SessionLabel) -> None:
         pass
 
-    def kill_session(self, session_name: str) -> None:
+    def kill_session(self, label: SessionLabel) -> None:
         pass
 
-    def delete_session(self, session_name: str) -> None:
+    def delete_session(self, label: SessionLabel) -> None:
         pass
 
-    def rename_session(self, session_name: str, new_name: str) -> None:
+    def rename_session(self, label: SessionLabel, new_name: str) -> None:
         pass
 
     def list_saved_sessions(self) -> List[SessionLabel]:
--- src/model.py
+++ src/model.py
@@ -1,12 +1,14 @@
 import abc
 from typing import List
 
+from src.file_handler import FileHandler
 from src.jmux_session import SessionLabel
+from src.multiplexer import Multiplexer
 
 
 class Model(abc.ABC):
     @abc.abstractmethod
-    def __init__(self):
+    def __init__(self, multiplexer: Multiplexer, file_handler: FileHandler) -> None:
         """
         Abstract class for the model of the appllication.
         Responsible for communicating with the presenter.
@@ -16,60 +18,61 @@ class Model(abc.ABC):
     @abc.abstractmethod
     def create_session(self, session_name: str) -> None:
         """
-        Create a new session with the name `session_name`.
+        Create a new session in the terminal multiplexer with the name `session_name`.
         """
         raise NotImplementedError
 
-    def save_session(self, session_name: str) -> None:
+    def save_session(self, label: SessionLabel) -> None:
         """
-        Save the session with the name `session_name`.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def load_session(self, session_name: str) -> None:
-        """
-        Load a session with the name `session_name`.
+        Save the session with `label` to a file.
         """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def kill_session(self, session_name: str) -> None:
+    def load_session(self, label: SessionLabel) -> None:
         """
-        Kill a session with the name `session_name`.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def delete_session(self, session_name: str) -> None:
-        """
-        Delete a session with the name `session_name`.
+        Load the session with `label` from a file.
         """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def rename_session(self, session_name: str, new_name: str) -> None:
+    def kill_session(self, label: SessionLabel) -> None:
         """
-        Rename a session with the name `session_name` to `new_name`.
+        Kills the session with `label` in the terminal multiplexer.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def delete_session(self, label: SessionLabel) -> None:
+        """
+        Delete the session with `label` from the file system.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rename_session(self, label: SessionLabel, new_name: str) -> None:
+        """
+        Rename the session with `label` to `new_name` in the multiplexer
+        and in the file system.
         """
         raise NotImplementedError
 
     @abc.abstractmethod
     def list_saved_sessions(self) -> List[SessionLabel]:
         """
-        List all saved sessions.
+        List all sessions saved in the file system.
         """
         raise NotImplementedError
 
     def list_running_sessions(self) -> List[SessionLabel]:
         """
-        List all running sessions.
+        List all sessions currently running in the terminal multiplexer.
         """
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_active_session(self) -> SessionLabel:
         """
-        Get the currently active session.
+        Get the currently active/focused session in the terminal multiplexer.
         """
         raise NotImplementedError
