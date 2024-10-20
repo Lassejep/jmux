8aabee90b863f79c94c877fde34845155707c53b
lassejep <lasse.jepsen@hotmail.com>
2024-10-20 20:53:31 +0200

    changed name from JmuxPresenter to CursesPresenter

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,7 +1,7 @@
-__all__ = ["CursesGui", "JsonHandler", "JmuxModel", "JmuxPresenter", "TmuxClient"]
+__all__ = ["CursesGui", "JsonHandler", "JmuxModel", "TmuxClient", "CursesPresenter"]
 
 from src.services.curses_gui import CursesGui
+from src.services.curses_presenter import CursesPresenter
 from src.services.jmux_model import JmuxModel
-from src.services.jmux_presenter import JmuxPresenter
 from src.services.json_handler import JsonHandler
 from src.services.tmux_client import TmuxClient
