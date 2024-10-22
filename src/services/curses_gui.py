b8a95eede162256c0f3fdd044f8862011bc26a29
lassejep <lasse.jepsen@hotmail.com>
2024-10-22 04:48:18 +0200

    renamed and moved message_box and running_sessions_menu into their own classes

--- src/services/curses_gui.py
+++ src/services/curses_gui.py
@@ -1,16 +1,13 @@
 import curses
-from typing import Callable, Concatenate, Dict, List, ParamSpec, Tuple, TypeVar
+from typing import Dict, List
 
 from src.interfaces import Model, Presenter, View
 from src.models import Commands
 from src.services.curses_presenter import CursesPresenter
 
-Params = ParamSpec("Params")
-ReturnType = TypeVar("ReturnType")
-
 
 class CursesGui(View):
-    def __init__(self, model: Model):
+    def __init__(self, model: Model) -> None:
         """
         A Curses GUI that implements the View interface.
         """
@@ -37,20 +34,6 @@ class CursesGui(View):
                 key_to_command[key] = command
         return key_to_command
 
-    @staticmethod
-    def _static_cursor(
-        func: Callable[Concatenate["CursesGui", Params], ReturnType]
-    ) -> Callable[Concatenate["CursesGui", Params], ReturnType]:
-        def inner(
-            self: "CursesGui", *args: Params.args, **kwargs: Params.kwargs
-        ) -> ReturnType:
-            cursor_pos = self.screen.getyx()
-            result = func(self, *args, **kwargs)
-            self.screen.move(*cursor_pos)
-            return result
-
-        return inner
-
     def start(self) -> None:
         """
         Start the view.
@@ -64,7 +47,7 @@ class CursesGui(View):
         curses.curs_set(0)
         curses.set_escdelay(50)
         self._init_colors()
-        self._init_screen(stdscr)
+        self._init_screens(stdscr)
         self.presenter.run()
 
     def _init_colors(self) -> None:
@@ -73,12 +56,23 @@ class CursesGui(View):
         curses.init_pair(1, curses.COLOR_RED, -1)
         self.error_color = curses.color_pair(1)
 
-    def _init_screen(self, stdscr) -> None:
-        size = stdscr.getmaxyx()
-        self.screen_size: Tuple[int, int] = size
-        self.screen: curses.window = curses.newpad(size[0] - 1, size[1])
-        self.screen.keypad(True)
-        self.msgbox = curses.newpad(1, size[1])
+    def _init_screens(self, window) -> None:
+        self.window_size = window.getmaxyx()
+        self.multiplexer_menu = MultiplexerMenu(window)
+        self.message_screen = MessageWindow(window)
+        self._draw_borders(window)
+
+    def _draw_borders(self, window: curses.window) -> None:
+        window.border()
+        window.hline(self.window_size[0] - 3, 0, curses.ACS_HLINE, self.window_size[1])
+        window.addch(self.window_size[0] - 3, 0, curses.ACS_LTEE)
+        window.addch(self.window_size[0] - 3, self.window_size[1] - 1, curses.ACS_RTEE)
+        window.vline(
+            0, self.window_size[1] // 2, curses.ACS_VLINE, self.window_size[0] - 3
+        )
+        window.addch(0, self.window_size[1] // 2, curses.ACS_TTEE)
+        window.addch(self.window_size[0] - 3, self.window_size[1] // 2, curses.ACS_BTEE)
+        window.refresh()
 
     def stop(self) -> None:
         """
@@ -91,144 +85,158 @@ class CursesGui(View):
         """
         Get user input.
         """
-        return self.commands.get(self.screen.getch(), Commands.UNKNOWN)
+        return self.commands.get(self.multiplexer_menu.screen.getch(), Commands.UNKNOWN)
 
     def show_menu(self, sessions: List[str]) -> None:
         """
         Show the menu.
         """
-        self.screen.clear()
-        self.screen.addstr(0, 0, "Select an action:")
-        for index, session in enumerate(sessions):
-            self.screen.addstr((index + 1), 0, session)
-        self.screen.move(self.presenter.position + 1, 0)
-        self.screen.chgat(self.presenter.position + 1, 0, -1, curses.A_REVERSE)
-        size = self.screen.getmaxyx()
-        self.screen.refresh(0, 0, 0, 0, size[0] - 1, size[1])
+        self.multiplexer_menu.show_menu(sessions, self.presenter.position)
+        self.multiplexer_menu._refresh()
 
-    @_static_cursor
     def show_error(self, message: str) -> None:
         """
         Show an error message.
         """
-        self.msgbox.clear()
-        self.msgbox.addstr(message, self.error_color)
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
+        self.message_screen.show_message(message, self.error_color)
 
     def cursor_down(self) -> None:
         """
         Move the cursor down.
         """
-        cursor_y, cursor_x = self.screen.getyx()
-        new_y = cursor_y + 1
-        self.screen.move(new_y, cursor_x)
-        self._move_highlight(cursor_y, new_y)
+        self.multiplexer_menu._move_down()
 
     def cursor_up(self) -> None:
         """
         Move the cursor up.
         """
-        cursor_y, cursor_x = self.screen.getyx()
-        new_y = cursor_y - 1
-        self.screen.move(new_y, cursor_x)
-        self._move_highlight(cursor_y, new_y)
+        self.multiplexer_menu._move_up()
 
-    def _move_highlight(self, old_y: int, new_y: int) -> None:
-        self.screen.chgat(old_y, 0, -1, curses.A_NORMAL)
-        self.screen.chgat(new_y, 0, -1, curses.A_REVERSE)
-
-    @_static_cursor
     def get_confirmation(self, message: str) -> bool:
         """
         Show the `message` and get return the key pressed.
         """
-        self.msgbox.clear()
-        self.msgbox.addstr(message)
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
-        confirmation = self.msgbox.getch() in [ord("y"), ord("Y")]
-        self.msgbox.clear()
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
-        return confirmation
+        self.message_screen.show_message(message)
+        return self.message_screen.get_confirmation()
 
-    @_static_cursor
     def create_new_session(self) -> str:
         """
         Show a prompt to create a new session and return the name of the session.
         """
-        self.msgbox.clear()
-        self.msgbox.addstr("Enter a name for the new session: ")
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
-        curses.echo()
-        curses.curs_set(1)
-        name = self.msgbox.getstr().decode("utf-8")
-        curses.curs_set(0)
-        curses.noecho()
-        self.msgbox.clear()
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
-        return name
+        self.message_screen.show_message("Enter a name for the new session: ")
+        return self.message_screen.get_input()
 
-    @_static_cursor
     def rename_session(self, session_name: str) -> str:
         """
         Show a prompt to rename a session and return the new name.
         """
-        self.msgbox.clear()
-        self.msgbox.addstr("Enter a new name for the session: ")
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
+        self.message_screen.show_message(f"Enter a new name for {session_name}: ")
+        return self.message_screen.get_input()
+
+
+class MessageWindow:
+    def __init__(self, window: curses.window) -> None:
+        """
+        A window to show messages.
+        """
+        self.window = window
+        self.window_size = window.getmaxyx()
+        self.size = (1, self.window_size[1] - 2)
+        self.position = (self.window_size[0] - 2, 1)
+        self.screen = curses.newpad(*self.size)
+        self.screen.keypad(True)
+
+    def _refresh(self) -> None:
+        lower_corner = (pos + size for pos, size in zip(self.position, self.size))
+        self.screen.refresh(0, 0, *self.position, *lower_corner)
+
+    def clear(self) -> None:
+        """
+        Clear the screen.
+        """
+        self.screen.clear()
+        self._refresh()
+
+    def show_message(self, message: str, color: int = curses.A_NORMAL) -> None:
+        """
+        Show a message.
+        """
+        self.screen.clear()
+        self.screen.addstr(message, color)
+        self._refresh()
+
+    def get_confirmation(self) -> bool:
+        """
+        Show a confirmation message.
+        """
+        confirmation = self.screen.getch() in [ord("y"), ord("Y")]
+        self.clear()
+        return confirmation
+
+    def get_input(self) -> str:
+        """
+        Get user input.
+        """
         curses.echo()
         curses.curs_set(1)
-        name = self.msgbox.getstr().decode("utf-8")
+        input = self.screen.getstr().decode("utf-8")
         curses.curs_set(0)
         curses.noecho()
-        self.msgbox.clear()
-        self.msgbox.refresh(
-            0,
-            0,
-            self.screen_size[0] - 1,
-            0,
-            self.screen_size[0],
-            self.screen_size[1],
-        )
-        return name
+        self.clear()
+        return input
+
+
+class MultiplexerMenu:
+    def __init__(self, window: curses.window) -> None:
+        """
+        A window to show a multiplexer menu.
+        """
+        self.window = window
+        self.window_size = window.getmaxyx()
+        self.size = (self.window_size[0] - 4, (self.window_size[1] - 2) // 2)
+        self.position = (1, 1)
+        self.screen = curses.newpad(*self.size)
+        self.screen.keypad(True)
+
+    def _refresh(self) -> None:
+        lower_corner = (pos + size for pos, size in zip(self.position, self.size))
+        self.screen.refresh(0, 0, *self.position, *lower_corner)
+
+    def clear(self) -> None:
+        """
+        Clear the screen.
+        """
+        self.screen.clear()
+        self._refresh()
+
+    def show_menu(self, sessions: List[str], position: int) -> None:
+        """
+        Show a menu.
+        """
+        self.screen.clear()
+        self.screen.addstr(0, 0, "Running Sessions:")
+        for index, session in enumerate(sessions):
+            self.screen.addstr(index + 1, 0, session)
+        self.screen.move(position + 1, 0)
+        self.screen.chgat(position + 1, 0, -1, curses.A_REVERSE)
+        self._refresh()
+
+    def _move_down(self) -> None:
+        """
+        Move the cursor down.
+        """
+        cursor_y, cursor_x = self.screen.getyx()
+        if cursor_y < self.size[0]:
+            self.screen.chgat(cursor_y, 0, -1, curses.A_NORMAL)
+            self.screen.move(cursor_y + 1, cursor_x)
+            self.screen.chgat(cursor_y + 1, 0, -1, curses.A_REVERSE)
+
+    def _move_up(self) -> None:
+        """
+        Move the cursor up.
+        """
+        cursor_y, cursor_x = self.screen.getyx()
+        if cursor_y > 0:
+            self.screen.chgat(cursor_y, 0, -1, curses.A_NORMAL)
+            self.screen.move(cursor_y - 1, cursor_x)
+            self.screen.chgat(cursor_y - 1, 0, -1, curses.A_REVERSE)
