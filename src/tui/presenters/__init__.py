023540cd963cb9c13eb97c219c05f863b3a8e46f
lassejep <lasse.jepsen@hotmail.com>
2024-10-31 17:22:24 +0100

    removed deprecated command_bar

--- src/tui/presenters/__init__.py
+++ src/tui/presenters/__init__.py
@@ -1,11 +1,9 @@
 __all__ = [
-    "CommandBarPresenter",
     "CursesPresenter",
     "MenuPresenter",
     "InputFieldPresenter",
 ]
 
-from .command_bar import CommandBarPresenter
 from .curses_presenter import CursesPresenter
 from .input_field import InputFieldPresenter
 from .menus import MenuPresenter
