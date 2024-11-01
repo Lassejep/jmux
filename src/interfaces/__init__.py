369d4b48aee4dd4025d4f6e01f40e1b673267def
lassejep <lasse.jepsen@hotmail.com>
2024-11-01 13:31:07 +0100

    small refactor to make code a bit cleaner

--- src/interfaces/__init__.py
+++ src/interfaces/__init__.py
@@ -4,12 +4,10 @@ __all__ = [
     "Presenter",
     "View",
     "FileHandler",
-    "SessionHandler",
 ]
 
 from .file_handler import FileHandler
 from .model import Model
 from .multiplexer import Multiplexer
 from .presenter import Presenter
-from .session_handler import SessionHandler
 from .view import View
